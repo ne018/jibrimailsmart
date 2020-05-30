f76aaaab878c8689f29b78f3bc8084579b136cd3
drey018 <drey01814@gmail.com>
2020-05-31 03:27:01 +0800

    Update takamata.py
    avoid assisting unprepared record

--- takamata.py
+++ takamata.py
@@ -16,9 +16,9 @@ from email import encoders
 
 print("work in progress...")
 
-dirPath = "/recordings"
-batt_field = "/var/www/html/recordings"
-serviceurl = "meet.example.com"
+dirPath = "/tmp/recordings"
+batt_field = "/var/www/recordings"
+serviceurl = "meet.domain.com"
 boshroomchkr = "http://%s:5280/room?room=%s&domain=%s&subdomain="
 
 sender_email = 'youremail'
@@ -94,80 +94,97 @@ if os.listdir(dirPath) != []:
     enemies = sorted(os.listdir('.'), key=os.path.getmtime)
 
     if enemies is not None:
-        target = enemies[0]
-        print("here's the target: ", target)
-        loc_target = (dirPath+"/"+target)
-        print("target location: ", loc_target)
+        victim = None
 
-#       prod
-        fieldWar = shutil.move(loc_target, batt_field)
-        print("field war: ",fieldWar)
+        for enemy in enemies:
+            if victim is not None:
+                break
 
-#       for testing
-#        fieldWar = "/var/www/html/recordings/uguxtmtcqlmlkwna"
+            os.chdir(dirPath+"/"+enemy)
+            scanmeta = os.listdir('.')
 
-        dirPrefix = str.split(fieldWar, '/var/www/html/recordings/')[1]
+            for fmeta in scanmeta:
+                if fmeta == "metadata.json":
+                    print("has a meta")
+                    victim = enemy
+                    break
 
-        if os.listdir(fieldWar) != []:
-            print("Meron")
-            os.chdir(fieldWar)
-            anatomy = os.listdir('.')
+        if victim is not None:
+            print(victim)
+            target = victim
+            print("here's the target: ", target)
+            loc_target = (dirPath+"/"+target)
+            print("target location: ", loc_target)
 
-            mod_emails = []
+#           prod
+            fieldWar = shutil.move(loc_target, batt_field)
+            print("field war: ",fieldWar)
 
-            for getneed in anatomy:
-                if getneed == "metadata.json":
+#           for testing
+#           fieldWar = "/var/www/html/recordings/uguxtmtcqlmlkwna"
 
-                    metadatafile = file_get_contents(getneed)
-                    getattr = json.loads(metadatafile)
-                    urlpath = urllib.parse.urlparse(getattr["meeting_url"]).path
-                    roomname = str.split(urlpath, '/')[1]
-                    print('roomname: '+roomname)
-                    skillatk = boshroomchkr % (serviceurl, roomname, serviceurl)
-                    req = requests.get(skillatk)
-                    if req.status_code == 200:
-                        boshresp = req.text
-                        print(boshresp)
-                        getlist = json.loads(boshresp)
+            dirPrefix = str.split(fieldWar, '/var/www/html/recordings/')[1]
 
-                        for item in getlist:
-                            itemstr = str(item)
-                            print(item)
+            if os.listdir(fieldWar) != []:
+                print("Meron")
+                os.chdir(fieldWar)
+                anatomy = os.listdir('.')
 
-                            # for organize condition
-                            if itemstr.find("'role': 'moderator'") == -1:
-                                print("participants")
-                            else:
-                                print("moderators")
-                                getresp = json.loads(itemstr.replace("'",'"'))
-                                #print("your_email: ",getresp['email'])
-                                if getresp['email'] != '':
-                                    mod_emails.append(getresp['email'])
-                                    print("moderator added..")
+                mod_emails = []
+
+                for getneed in anatomy:
+                    if getneed == "metadata.json":
+
+                        metadatafile = file_get_contents(getneed)
+                        getattr = json.loads(metadatafile)
+                        urlpath = urllib.parse.urlparse(getattr["meeting_url"]).path
+                        roomname = str.split(urlpath, '/')[1]
+                        print('roomname: '+roomname)
+                        skillatk = boshroomchkr % (serviceurl, roomname, serviceurl)
+                        req = requests.get(skillatk)
+                        if req.status_code == 200:
+                            boshresp = req.text
+                            print(boshresp)
+                            getlist = json.loads(boshresp)
+
+                            for item in getlist:
+                                itemstr = str(item)
+                                print(item)
+
+                                # for organize condition
+                                if itemstr.find("'role': 'moderator'") == -1:
+                                    print("participants")
                                 else:
-                                    print('moderator no existing email')
+                                    print("moderators")
+                                    getresp = json.loads(itemstr.replace("'",'"'))
+                                    #print("your_email: ",getresp['email'])
+                                    if getresp['email'] != '':
+                                        mod_emails.append(getresp['email'])
+                                        print("moderator added..")
+                                    else:
+                                        print('moderator no existing email')
 
-                            #for sureball
-                            #getresp = json.loads(itemstr.replace("'",'"'))
-                            #print("your_email: ",getresp['email'])
-                            #if getresp['email'] != "":
-                            #mod_emails.append(getresp['email'])
+                                #for sureball
+                                #getresp = json.loads(itemstr.replace("'",'"'))
+                                #print("your_email: ",getresp['email'])
+                                #if getresp['email'] != "":
+                                #mod_emails.append(getresp['email'])
+                        else:
+                            print('roomname not found')
+
                     else:
-                        print('roomname not found')
-
-                else:
-                    videoname = getneed
+                        videoname = getneed
 
 
-            print(videoname)
-            print(mod_emails)
-            if mod_emails != []:
-                urllink = 'https://'+serviceurl+'/recordings/'+dirPrefix+'/'+videoname
-                for mod in mod_emails:
-                    print("moderator: ",mod)
+                print(videoname)
+                print(mod_emails)
+                if mod_emails != []:
+                    urllink = 'https://'+serviceurl+'/recordings/'+dirPrefix+'/'+videoname
+                    for mod in mod_emails:
+                        print("moderator: ",mod)
 
-                    send_mail(mod, sender_email, sender_passwd, urllink, roomname)
-                    print("mail sent to ",mod)
+                        send_mail(mod, sender_email, sender_passwd, urllink, roomname)
+                        print("mail sent to ",mod)
 
 else:
     print('no files found in recording file path')
